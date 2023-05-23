298348cd1597466a0ecdb9fb142f381f1703b1ee
Ashini <ashini@surge.global>
2023-05-23 10:09:30 +0530

    initial coomt

---
+++ src/test/java/com/sample/boilerplate/BoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
