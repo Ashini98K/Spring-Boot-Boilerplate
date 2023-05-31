1e7bdf623c6cf353c42a8ef8686e915781856ddc
Pasindu Bhasura <pasindu.b@surge.global>
2023-05-31 17:06:48 +0530

    add comments

--- src/main/java/com/sample/boilerplate/exceptions/ApiError.java
+++ src/main/java/com/sample/boilerplate/exceptions/ApiError.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+/**
+ * Represents an error response model for API exceptions.
+ */
 @Getter
 @Setter
 public class ApiError {
@@ -15,12 +18,25 @@ public class ApiError {
     private ZonedDateTime dateTime;
     private List<FieldError> fieldErrors;
 
+    /**
+     * Constructs an instance of ApiError with HTTP status, message, and timestamp.
+     * @param httpStatus The HTTP status code of the error response
+     * @param message A descriptive message for the error
+     * @param dateTime The timestamp when the error occurred
+     */
     public ApiError(HttpStatus httpStatus, String message, ZonedDateTime dateTime) {
         this.httpStatus = httpStatus;
         this.message = message;
         this.dateTime = dateTime;
     }
 
+    /**
+     * Constructs an instance of ApiError with HTTP status, message, timestamp, and field errors.
+     * @param httpStatus The HTTP status code of the error response
+     * @param message A descriptive message for the error
+     * @param dateTime The timestamp when the error occurred
+     * @param fieldErrors A list of field-specific error details
+     */
     public ApiError(HttpStatus httpStatus, String message, ZonedDateTime dateTime, List<FieldError> fieldErrors) {
         this.httpStatus = httpStatus;
         this.message = message;
